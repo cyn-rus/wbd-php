e84e305c5520c9ac2fef827e1f9d0757ad68458e
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-09-29 22:49:37 +0700

    init components

---
+++ client/components/header.js
@@ -0,0 +1,12 @@
+// versi admin & user
+// admin: search bar, tambah varian (add), logout
+// user: search bar, daftar pembelian (bonus), logout
+
+// isAdmin: boolean
+function renderHeader(isAdmin) {
+    return (
+        <div>
+
+        </div>
+    )
+}
