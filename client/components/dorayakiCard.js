9644d62306b4ba6b842dd4da08c566b19c08355f
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-09 00:56:11 +0700

    add script for dorayaki card (to be used on dashboard and search page)

--- client/components/dorayakiCard.js
+++ client/components/dorayakiCard.js
@@ -1,3 +1,10 @@
-function renderDorayakiCard() {
-    
+function renderDorayakiCard(name, desc, sold, image) {
+    let child = `<img src="${image}">
+                <div>${name}</div>
+                <div>${desc}</div>
+                <div>${sold}</div>`
+}
+
+export default function renderDorayakiContainer() {
+
 }
