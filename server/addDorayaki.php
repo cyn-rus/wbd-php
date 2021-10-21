36afa1bf24167cf341dbdb2ae0eb8ba75d7f9faa
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-22 01:46:41 +0700

    add dorayaki on BE

--- server/addDorayaki.php
+++ server/addDorayaki.php
@@ -2,7 +2,6 @@
     if (isset($_POST['submit'])) {
         $dorayakiName = $_REQUEST['inputDorayakiName'];
         $dorayakiDescription = $_REQUEST['inputDorayakiDescription'];
-        $dorayakiImageTmp = $_REQUEST['inputDorayakiImage'];
         $dorayakiPrice = $_REQUEST['inputDorayakiPrice'];
         $dorayakiStock = $_REQUEST['inputDorayakiStock'];
 
@@ -19,12 +18,20 @@
         $row = $res->fetchArray();
         $dorayakiId = $row['dorayaki_id_max'];
 
-        $dorayakiImage = "./images/" . $dorayakiImageTmp;
+        $filename = $_FILES['uploadfile']['name'];
+        $tempname = $_FILES['uploadfile']['tmp_name'];
+        $folder = "../db/images/".$filename;
 
-        echo $dorayakiId;
+        if (!empty($dorayakiName) and !empty($dorayakiDescription) and !empty($dorayakiPrice) and !empty($dorayakiStock) and $empty($filename)) {
+            if (move_uploaded_file($tempname, $folder)) {
+                $sql = "INSERT INTO dorayakis VALUES(". $dorayakiId . ",'" . $dorayakiName . "','" . $dorayakiDescription . "','" . $filename . "'," . $dorayakiPrice . "," . $dorayakiStock . ");";
+    
+                $ret = $db->exec($sql);
 
-        $sql = "INSERT INTO dorayakis VALUES(". $dorayakiId . ",'" . $dorayakiName . "','" . $dorayakiDescription . "','" . $dorayakiImage . "'," . $dorayakiPrice . "," . $dorayakiStock . ");";
-        echo $sql;
-        $ret = $db->exec($sql);
+                header('Location: ../client/pages/dashboard.html');
+            }
+        } else {
+            header('Location: ../client/pages/add.html');
+        }
     }
 ?>
