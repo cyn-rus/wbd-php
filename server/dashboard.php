9221224edb93efe547306399cfb59c5c05c82f80
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-22 01:47:29 +0700

    add get dorayakis for dashboard function in BE

--- server/dashboard.php
+++ server/dashboard.php
@@ -2,14 +2,21 @@
     $maxDorayaki = $_REQUEST['max'];
     // echo $maxDorayaki;
     $db = new SQLite3('../db/doraemonangis.sq3');
-    $sql = "SELECT dorayakis.dorayaki_name, amount, description, image FROM dorayakis, transactions WHERE dorayakis.dorayaki_name = transactions.dorayaki_name ORDER BY amount DESC LIMIT $maxDorayaki";
+    $sql = "SELECT dorayaki_id, dorayakis.dorayaki_name, IFNULL(SUM(amount), 0) as sold, description, image 
+            FROM dorayakis
+            LEFT OUTER JOIN transactions
+            ON dorayakis.dorayaki_name = transactions.dorayaki_name 
+            GROUP BY dorayaki_id 
+            ORDER BY SUM(amount) DESC 
+            LIMIT $maxDorayaki";
     $result = $db->query($sql);
 
     $popularDorayakis = [];
         while ($row = $result->fetchArray()) {
             $popularDorayakis[] = [
+                'id' => $row['dorayaki_id'],
                 'name' => $row['dorayaki_name'],
-                'amount' => $row['amount'],
+                'sold' => $row['sold'],
                 'description' => $row['description'],
                 'image' => $row['image'],
             ];
