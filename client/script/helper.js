f73d6e720838f2739e143ca1f92b7e4e3aacfc58
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-22 01:52:06 +0700

    add capitalizeSentence, getCookie, bubbleSort as helper functions

--- client/script/helper.js
+++ client/script/helper.js
@@ -33,3 +33,45 @@ function parseDataFromAJAX(string) {
     const parsed = JSON.parse(string.slice(0, -2))
     return parsed
 }
+
+function capitalizeSentence(string) {
+    const words = string.split(' ')
+
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1)
+    }
+    
+    return words.join(' ')
+}
+
+function getCookie(cookieName) {
+    let name = cookieName + "="
+    let cookies = document.cookie.split(';')
+
+    for (let i = 0; i < cookies.length; i++) {
+        let c = cookies[i];
+
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+
+    return ""
+}
+
+function bubbleSort(arr, key) {
+    for (let i = 0; i < arr.length-1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j][key] > arr[j+1][key]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr
+}
