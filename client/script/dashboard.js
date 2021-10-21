d2395424a8999c07903b7066f843055dffea8291
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-22 01:51:36 +0700

    refactor dashboard script

--- client/script/dashboard.js
+++ client/script/dashboard.js
@@ -1,13 +1,16 @@
-// import renderDorayakiContainer from "../components/dorayakiCard";
-
-
 // Menampilkan username pengguna
-const username = document.getElementById("username");
-let usernameCookie = decodeURIComponent(document.cookie);
+const username = document.getElementById("username")
+const cookie = decodeURIComponent(document.cookie);
+// const usernameCookie = (cookie.split(';'))[0]
+
 // let usernameCookie = document.cookie;
-let x = usernameCookie.split('=');
-let usernameFromCookie = x[1];
-const child = `<div>${usernameFromCookie}<\div>`
+// let x = usernameCookie.split('=');
+// console.log(usernameCookie)
+// const usernameFromCookie = (usernameCookie.split('='))[1]
+const usernameFromCookie = getCookie("username")
+
+// let usernameFromCookie = usernameCookie["username"];
+const child = `<h3>Username  ${usernameFromCookie}</h3>`
 username.innerHTML = child;
 
 
@@ -19,27 +22,12 @@ async function renderDashboardDorayaki(){
         .then(data =>{
             return parseDataFromAJAX(data)
         })
-    console.log(popularDorayakis)
-    function renderPopularDorayakiDisplay(){
-        const popularDorayakiDisplay = document.getElementById('popularDorayakiDisplay')
-        for (let i = 0; i < maxDisplayedDorayaki && i < popularDorayakis.length; i++){
-            popularDorayakiDisplay.innerHTML += popularDorayakis[i].name + '<br>'
-            popularDorayakiDisplay.innerHTML += popularDorayakis[i].amount + '<br>'
-            console.log(popularDorayakis[i].name)
-        }
-    }
 
-    renderPopularDorayakiDisplay()
+    const container = document.getElementById('container')
+    container.innerHTML = ''
+    for (let i = 0; i < maxDisplayedDorayaki && i < popularDorayakis.length; i++) {
+        container.innerHTML += renderDorayakiCard(popularDorayakis[i])
+    }
 }
 
 renderDashboardDorayaki()
-
-// function topDorayakiDisplay(){
-//     for (i = 0; i < maxDisplayedDorayaki; i++)
-//     {
-        
-//     }
-// }
-
-
-
