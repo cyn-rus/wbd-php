47e19eed1f612efcbfff4a71d743191c1b5c8564
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-22 01:51:10 +0700

    refactor renderDorayakiCard function

--- client/components/dorayakiCard.js
+++ client/components/dorayakiCard.js
@@ -1,6 +1,10 @@
-function renderDorayakiCard(name, desc, sold, image) {
-    let child = `<img src="${image}">
-                <div>${name}</div>
-                <div>${desc}</div>
-                <div>${sold}</div>`
+function renderDorayakiCard(dorayaki, searchPage) {
+    return `
+        <div class='dorayaki-card' onclick='window.location.href="dorayaki.html?id=${dorayaki.id}"'>
+            <img src='../../db/images/${dorayaki.image}' alt=${capitalizeSentence(dorayaki.name)}>
+            <h1 class='dorayaki-name'>${capitalizeSentence(dorayaki.name)}</h1>
+            <h3 class='dorayaki-desc'>${dorayaki.description}</h3>
+            ${searchPage !== true ? `<h3 class='dorayaki-sold'>Sold: ${dorayaki.sold}</h3>` : ''}
+        </div> 
+    `
 }
