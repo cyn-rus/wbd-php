7bad55537fd087fd924557f0cdb9c2324514f93e
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-22 11:55:48 +0700

    add total sold for search dorayaki to render

--- server/searchDorayaki.php
+++ server/searchDorayaki.php
@@ -4,14 +4,20 @@
 
     if ($dorayaki !== '') {
         $db = new SQLite3('../db/doraemonangis.sq3');
-        $sql = "SELECT dorayaki_name, price, description, image FROM dorayakis WHERE dorayaki_name LIKE '%" . $dorayaki . "%'";
+        $sql = "SELECT dorayaki_id, dorayakis.dorayaki_name, IFNULL(SUM(amount), 0) as sold, description, image
+                FROM dorayakis LEFT OUTER JOIN transactions ON dorayakis.dorayaki_name = transactions.dorayaki_name
+                WHERE dorayakis.dorayaki_name LIKE '%" . $dorayaki . "%'
+                GROUP BY dorayaki_id
+                ORDER BY SUM(amount) DESC";
+
         $result = $db->query($sql);
     
         $dorayakis = [];
         while ($row = $result->fetchArray()) {
             $dorayakis[] = [
+                'id' => $row['dorayaki_id'],
                 'name' => $row['dorayaki_name'],
-                'price' => $row['price'],
+                'sold' => $row['sold'],
                 'description' => $row['description'],
                 'image' => $row['image'],
             ];
