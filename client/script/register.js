e4f160f5532d8448b6b861f90736defb69c39c94
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-22 11:54:05 +0700

    add email checking when registering

--- client/script/register.js
+++ client/script/register.js
@@ -1,28 +1,59 @@
+function checkUsername(str) {
+    const usernameRegex = /^[a-zA-Z0-9_]*$/
+    if (str.includes(' ')) return false
+    if (str === '') return false
+    if (!usernameRegex.test(String(str))) return false
+    return true
+}
+
 async function checkAvailability() {
     const inputUsername = document.getElementById('inputUsername')
-    var username = inputUsername.value
+    const username = inputUsername.value
 
     const availability = await fetchDatas(`check_availability.php?q=${username}`)
         .then(data => {
-            return JSON.parse(data)
+            try {
+                return JSON.parse(data)
+            } catch {
+                return ''
+            }
         })
 
-    const found = availability.found
 
+    const found = !(availability.found) && checkUsername(username)
+    
     const status = document.getElementById('status')
-    status.innerHTML = ''
-    const tmpStatus = document.createElement('p')
 
-    if (found === 1) {
-        tmpStatus.innerHTML = 'Username not available'
-        document.getElementById('inputUsername').style.border = "2px solid red"
-        status.appendChild(tmpStatus)
+    
+    if (!found) {
+        inputUsername.style.border = "2px solid red"
+        status.style.opacity = 1
     } else {
-        tmpStatus.innerHTML = 'Username available'
-        document.getElementById('inputUsername').style.border = "2px solid rgb(87, 247, 111)"
-        status.appendChild(tmpStatus)
+        inputUsername.style.border = "2px solid rgb(87, 247, 111)"
+        status.style.opacity = 0
+    }
+}
+
+function checkEmail() {
+    const validity = document.getElementById('validity')
+
+    const inputEmail = document.getElementById('inputEmail')
+    const email = inputEmail.value
+
+    const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    const isValid = emailRegex.test(String(email).toLowerCase())
+
+    if (isValid) {
+        
+        inputEmail.style.border = '2px solid rgb(87, 247, 111)'
+        validity.style.opacity = 0
+    } else {
+        inputEmail.style.border = '2px solid red'
+        validity.style.opacity = 1
     }
 }
 
 const inputUsername = document.getElementById('inputUsername')
 inputUsername.onblur = function() {checkAvailability()}
+const inputEmail = document.getElementById('inputEmail')
+inputEmail.onblur = function() {checkEmail()}
