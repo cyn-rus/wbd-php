ae9c046d9c3067c202f9566fc8a9a63f500ec4ba
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-22 11:53:14 +0700

    refactor renderDorayakiCard

--- client/components/dorayakiCard.js
+++ client/components/dorayakiCard.js
@@ -1,10 +1,11 @@
-function renderDorayakiCard(dorayaki, searchPage) {
+function renderDorayakiCard(dorayaki) {
+    const imgPath = '../../db/images/'
     return `
         <div class='dorayaki-card' onclick='window.location.href="dorayaki.html?id=${dorayaki.id}"'>
-            <img src='../../db/images/${dorayaki.image}' alt=${capitalizeSentence(dorayaki.name)}>
+            <img src='${imgPath + dorayaki.image}' alt=${capitalizeSentence(dorayaki.name)}>
             <h1 class='dorayaki-name'>${capitalizeSentence(dorayaki.name)}</h1>
             <h3 class='dorayaki-desc'>${dorayaki.description}</h3>
-            ${searchPage !== true ? `<h3 class='dorayaki-sold'>Sold: ${dorayaki.sold}</h3>` : ''}
+            <h3 class='dorayaki-sold'>Sold: ${dorayaki.sold}</h3>
         </div> 
     `
 }
