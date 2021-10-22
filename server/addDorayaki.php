b47c1f7d035d9631f8a365355160c451f11ceef6
Cynthia Rusadi <cyn.rusadi@gmail.com>
2021-10-22 11:55:26 +0700

    debug on adding dorayaki

--- server/addDorayaki.php
+++ server/addDorayaki.php
@@ -13,20 +13,22 @@
             echo "Open database success...\n";
         }
 
-        $res = $db->query("SELECT COUNT(dorayaki_id) AS dorayaki_id_max FROM dorayakis");
+        $res = $db->query("SELECT MAX(dorayaki_id) AS dorayaki_id_max FROM dorayakis");
         
         $row = $res->fetchArray();
-        $dorayakiId = $row['dorayaki_id_max'];
+        $dorayakiId = $row['dorayaki_id_max'] + 1;
 
         $filename = $_FILES['uploadfile']['name'];
         $tempname = $_FILES['uploadfile']['tmp_name'];
         $folder = "../db/images/".$filename;
 
-        if (!empty($dorayakiName) and !empty($dorayakiDescription) and !empty($dorayakiPrice) and !empty($dorayakiStock) and $empty($filename)) {
+        if (!empty($dorayakiName) and !empty($dorayakiDescription) and !empty($dorayakiPrice) and !empty($dorayakiStock) and !empty($filename)) {
             if (move_uploaded_file($tempname, $folder)) {
-                $sql = "INSERT INTO dorayakis VALUES(". $dorayakiId . ",'" . $dorayakiName . "','" . $dorayakiDescription . "','" . $filename . "'," . $dorayakiPrice . "," . $dorayakiStock . ");";
-    
+                $sql = "INSERT INTO dorayakis VALUES('$dorayakiId','" . $dorayakiName . "','" . $dorayakiDescription . "','" . $filename . "'," . $dorayakiPrice . "," . $dorayakiStock . ");";
+
                 $ret = $db->exec($sql);
+
+                echo $db->lastErrorMsg();
 
                 header('Location: ../client/pages/dashboard.html');
             }
